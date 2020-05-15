559d644edb789f25cc02ecad17a609aa5c9d5ede
Pankaj <pgho580@aucklanduni.ac.nz>
2020-05-15 18:23:34 +1200

    created the project

---
+++ WebAPI/Controllers/Log_in.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class Log_in : Controller
+    {
+        // GET
+        [HttpGet]
+        public void Index()
+        {
+            return;
+        }
+    }
+}
---
+++ WebAPI/Controllers/Sign_in.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class Sign_in : Controller
+    {
+        // GET
+        [HttpGet]
+        public void Index()
+        {
+            return ;
+        }
+    }
+}
